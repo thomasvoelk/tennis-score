bdfdd1c7d8b555f637a6dabe8062a7ef884c47a5
thomas <thomas.voelk@gmail.com>
2015-04-25 17:41:25 +0200

    Score class is using player objects

--- src/main/java/tennis/Player.java
+++ src/main/java/tennis/Player.java
@@ -6,4 +6,21 @@ public class Player {
     public Player(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        if (name != null ? !name.equals(player.name) : player.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
--- src/main/java/tennis/Score.java
+++ src/main/java/tennis/Score.java
@@ -1,34 +1,40 @@
 package tennis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Score {
     private final Player player1;
     private final Player player2;
-    private int player1Points;
-    private int player2Points;
+    private final Map<Player, Integer> points = new HashMap<>(2);
 
     public Score(Player player1, Player player2) {
+        points.put(player1, 0);
+        points.put(player2, 0);
         this.player1 = player1;
         this.player2 = player2;
     }
 
     public int getPlayer1Points() {
-        return player1Points;
+        return pointsForPlayer(player1);
     }
 
     public void addPlayer1Point() {
-        player1Points++;
+        points.put(player1, pointsForPlayer(player1) + 1);
     }
 
     public int getPlayer2Points() {
-        return player2Points;
+        return pointsForPlayer(player2);
     }
 
     public void addPlayer2Point() {
-        player2Points++;
+        points.put(player2, pointsForPlayer(player2) + 1);
     }
 
     @Override
     public String toString() {
+        int player1Points = pointsForPlayer(player1);
+        int player2Points = pointsForPlayer(player2);
         String score1 = pointsToString(player1Points);
         String score2 = pointsToString(player2Points);
         if (player1Points != player2Points) {
@@ -38,6 +44,10 @@ public class Score {
         } else {
             return "Deuce";
         }
+    }
+
+    private int pointsForPlayer(Player player ) {
+        return points.get(player);
     }
 
     private String pointsToString(int points) {
